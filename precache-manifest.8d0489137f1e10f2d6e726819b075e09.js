f462dc6f7ad5d4bf7e45ac3231ade7b29ac8ea5a
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-06-19 19:12:24 +0530

    Updates

---
+++ precache-manifest.8d0489137f1e10f2d6e726819b075e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8964c43aba7c7bc69d988b814bf7f1b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fc3144b26de7400a4da",
+    "url": "/monsters-rolodex/static/css/main.7cbdf2eb.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc3144b26de7400a4da",
+    "url": "/monsters-rolodex/static/js/main.ae85dc5b.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
